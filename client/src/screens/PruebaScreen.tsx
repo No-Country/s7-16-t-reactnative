a0b9002116e54a792d56b3d0401271b58a2cb274
tomasAybar <aybar.t4@gmail.com>
2023-03-26 12:03:23 -0300

    pruebaScreen

---
+++ client/src/screens/PruebaScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const PruebaScreen = () => {
+	return (
+		<View>
+			<Text>PruebaScreen</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({})

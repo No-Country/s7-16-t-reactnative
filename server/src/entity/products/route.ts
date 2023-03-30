78879a2e011d9c5edd112ae4706256643b964b2b
Juan Fernandez <jmf798@gmail.com>
2023-03-30 02:16:26 -0300

    Add product model, controller anda services. Add database connect

--- server/src/entity/products/route.ts
+++ server/src/entity/products/route.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import { processRequestBody } from 'zod-express-middleware';
-import { StatusCodes } from 'http-status-codes';
+import { newProductSchema } from './schema';
+import { getProductHandler, getProductsHandler, newProductHandler, updateProductHandler } from './controller';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.status(StatusCodes.OK).json('GET-Product route');
-});
+router.get('/', getProductsHandler);
 
-router.post('/', (req, res) => {
-  return res.status(StatusCodes.CREATED).json('POST-Product route');
-});
+router.get('/:barCode', getProductHandler);
+
+router.post('/', processRequestBody(newProductSchema.body), newProductHandler);
+
+router.put('/:id', processRequestBody(newProductSchema.body), updateProductHandler);
+
+router.delete('/:id');
 
 export default router;

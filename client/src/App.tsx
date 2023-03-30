723c72f0557ea80126945e931ea7f9114911ddae
VictorMRJ <victortecno98@gmail.com>
2023-03-30 04:26:22 -0600

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  const [data, setData] = useState(10);
+
+  const num1 = 20;
+
+  if (num1 == data) {
+    console.log("num1 == data");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

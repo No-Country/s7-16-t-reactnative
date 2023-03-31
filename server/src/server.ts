67717d89b64bc2b9a5398072fcf7269307df2a0c
Miguel Vera <miguelverap01@gmail.com>
2023-03-30 22:12:14 -0500

    created register and login

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 import morgan from "morgan"
 import { deserializeUser } from './middleware';
 import router from './entity/routes';
 import { CORS_ORIGIN, PORT } from './config';
+import { setupRoutes } from './config/setupRoutes';
 
-const app = express();
+const app: Application = express();
 app.use(morgan('tiny'))
 app.use(express.json());
 app.use(
@@ -13,6 +14,8 @@ app.use(
     origin: CORS_ORIGIN,
   }),
 );
+
+setupRoutes(app)
 
 app.use(deserializeUser);
 

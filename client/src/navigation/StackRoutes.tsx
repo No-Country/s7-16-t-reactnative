d1a8ef3ff57964c11afb4f3aebdff99af313e141
diegonicita <diegonicita@gmail.com>
2023-04-01 18:50:05 -0300

    fix: Test some configuration for prettier

--- client/src/navigation/StackRoutes.tsx
+++ client/src/navigation/StackRoutes.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { TestStep1 }  from '../screens/TestStep1'
-import { TestStep2 }  from '../screens/TestStep2'
-import { TestStep3 }  from '../screens/TestStep3'
-import { TestStep4 }  from '../screens/TestStep4'
+import { TestStep1 } from '../screens/TestStep1'
+import { TestStep2 } from '../screens/TestStep2'
+import { TestStep3 } from '../screens/TestStep3'
+import { TestStep4 } from '../screens/TestStep4'
 import {
   NavigationContainer,
   DefaultTheme,
@@ -14,12 +14,12 @@ const Stack = createNativeStackNavigator()
 
 export const StackRoutes = () => {
   return (
-    <NavigationContainer theme={DefaultTheme}>
+    <NavigationContainer theme={DefaultTheme} >
       <Stack.Navigator>
-        <Stack.Screen name="TestStep1Name" component={TestStep1} />            
-        <Stack.Screen name="TestStep2Name" component={TestStep2} />            
-        <Stack.Screen name="TestStep3Name" component={TestStep3} />            
-        <Stack.Screen name="TestStep4Name" component={TestStep4} />            
+        <Stack.Screen name="TestStep1Name" component={TestStep1} />
+        <Stack.Screen name="TestStep2Name" component={TestStep2} />
+        <Stack.Screen name="TestStep3Name" component={TestStep3} />
+        <Stack.Screen name="TestStep4Name" component={TestStep4} />
       </Stack.Navigator>
     </NavigationContainer>
   )

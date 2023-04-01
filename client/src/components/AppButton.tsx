3784a79b809e6033a89f7b081bb769b720160634
tomasAybar <aybar.t4@gmail.com>
2023-04-01 11:38:53 -0300

    feat(screen): agregar pantalla "Onboarding3Screen"
    Crea pantalla Onboarding3Screen como base para futuros diseños y navegación.

--- client/src/components/AppButton.tsx
+++ client/src/components/AppButton.tsx
@@ -1,38 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
-
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 interface Props {
-    text: string
-    onPress: () => void
+  text: string;
+  onPress: () => void;
 }
 
 export const AppButton = ({ text, onPress }: Props) => {
-    return (
-        <TouchableOpacity style={styles.touchable} onPress={onPress}>
-            <Text style={styles.text}>{text}</Text>
-        </TouchableOpacity>
-    )
-}
+  return (
+    <TouchableOpacity style={styles.touchable} onPress={onPress}>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    touchable:{
-        width: 328,
-		height: 39,
-		borderRadius: 10,
-		alignItems: 'center',
-		justifyContent: 'center',
-        borderWidth: 0.5,
-        padding: 10,
-    },
-    text:{
-        fontSize: 16,
-        textAlign: 'center',
-        fontFamily: 'Roboto',
-        color: '#000000',
-        fontWeight: '400',
-        lineHeight: 19,
-        width: 67,
-        height: 19,
-    }
-})
+  touchable: {
+    width: 328,
+    height: 39,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 0.5,
+    padding: 10,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: "center",
+    fontFamily: "Roboto",
+    color: "#000000",
+    fontWeight: "400",
+    lineHeight: 19,
+    width: 67,
+    height: 19,
+  },
+});

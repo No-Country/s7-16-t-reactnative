5f839f436c7a4ae0bdd2642586e39df4c52b32d2
VictorMRJ <victortecno98@gmail.com>
2023-04-04 16:11:55 -0600

    feat: input reusable without style 100%

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 
 import { StackRoutes } from "./src/navigation/StackRoutes";
 import Example from "./src/Example";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-orange-600">
+    <View className="flex-1 items-center justify-center bg-white">
       <Example />
       <StatusBar style="auto" />
     </View>
--- client/src/Example.tsx
+++ client/src/Example.tsx
@@ -1,12 +1,51 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import React from "react";
+import { Formik } from "formik";
+import InputComponent from "./components/InputComponent";
+import { Text, View, TouchableOpacity } from "react-native";
+import { AppButton } from "./components/AppButton";
+interface MyFormValues {
+  firstName: string;
+  password: string;
+}
 
-const Example = () => {
+const Example: React.FC = () => {
+  const initialValues: MyFormValues = { firstName: "", password: "" };
   return (
-    <View className="bg-yellow-500">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="w-full">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View className=" bg-white flex flex-col gap-5 justify-center">
+            <View className="flex pl-5 pr-5">
+              <InputComponent
+                // type={true}
+                label="Username"
+                name="firstName"
+                placeholder="Username"
+                onChangeText={handleChange("firstName")}
+                onBlur={handleBlur("firstName")}
+                value={values.firstName}
+              />
+              <InputComponent
+                type={true}
+                label="Password"
+                name="password"
+                placeholder="Password"
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+              />
+            </View>
+            {/* <TouchableOpacity onPress={() => handleSubmit()}>
+              <Text>PRESIONAR</Text>
+            </TouchableOpacity> */}
+            <View className="flex justify-center items-center">
+              <AppButton text="PRESIONAR" onPress={() => handleSubmit()} />
+            </View>
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };

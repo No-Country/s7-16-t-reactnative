1eb5b22a1f577f55022d2d7741138a83445092ce
diegonicita <diegonicita@gmail.com>
2023-04-06 18:37:47 -0300

    fix: change some styles in login

--- client/src/components/LoginButton.tsx
+++ client/src/components/LoginButton.tsx
@@ -23,57 +23,73 @@ export const NormalBtn = ({ text, onPress }: Props) => {
 export const GoogleBtn = ({ text, onPress }: Props) => {
   return (
     <TouchableOpacity style={styles.googleBtn} onPress={onPress}>
-      <Text style={styles.googleText}>
-        <Image
-          source={require("../assets/google-logo.png")}
-          style={styles.image}
-        />
-        {text}
-      </Text>
+      <Text style={styles.googleText}>{text}</Text>
+      <Image
+        source={require("../assets/google-logo.png")}
+        style={styles.image}
+      />
     </TouchableOpacity>
   );
 };
 const styles = StyleSheet.create({
   normalBtn: {
-    width: 135,
+    width: 146,
     height: 36,
-    borderRadius: 4,
+    borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
     padding: 6,
+    paddingLeft: 8,
     backgroundColor: "hsla(0, 0%, 56%, 1)",
     margin: "5%",
   },
   normalText: {
     fontSize: 14,
+    lineHeight: 16,
+    letterSpacing: 1.25,
     textAlign: "center",
     color: "hsla(0, 0%, 90%, 1)",
     fontWeight: "500",
   },
   googleBtn: {
-    width: 265,
-    height: 40,
+    position: "relative",
+    backgroundColor: "white",
+    fontSize: 14,
+    width: 244,
+    height: 37.91,
     borderRadius: 4,
+    letterSpacing: 1.25,
+    lineHeight: 16,
     alignItems: "center",
     justifyContent: "center",
     borderWidth: 1,
     borderColor: "hsla(212, 100%, 38%, 0.5)",
+    fontWeight: "500",
   },
   googleText: {
-    fontSize: 14,
-    textAlign: "center",
+    position: "absolute",
+    fontSize: 16,
+    left: 39,
+    right: 6,
+    top: 7,
     color: "hsla(212, 100%, 38%, 1)",
     fontWeight: "500",
   },
   image: {
-    marginLeft: 5,
-    width: 20,
-    height: 20,
+    position: "absolute",
+    top: "6.25%",
+    left: "3.69%",
+    right: "86.07%",
+    bottom: "15.62%",
+    width: 30,
+    height: 30,
+    margin: 0,
+    padding: 0,
   },
   SessionBtn: {
-    width: 135,
+    width: 146,
     height: 36,
-    borderRadius: 4,
+    borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
     padding: 6,
--- client/src/screens/Login/MainLogin.tsx
+++ client/src/screens/Login/MainLogin.tsx
@@ -1,26 +1,28 @@
-import { Text, View, StyleSheet, Image } from "react-native";
+import { ScrollView, Text, View, StyleSheet, Image } from "react-native";
 import { NormalBtn, GoogleBtn, SessionBtn } from "../../components/LoginButton";
 
 export const MainLogin = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>¡Bienvenido a SmartShop!</Text>
-      <Image source={require("../../assets/logo.png")} style={{ margin: 40 }} />
-      <View style={styles.buttons}>
-        <SessionBtn
-          text="INICIAR SESIÓN"
-          onPress={() => navigation.navigate("Login")}
-        />
-        <NormalBtn
-          text="REGISTRARME"
-          onPress={() => navigation.navigate("Register1")}
-        />
-        <GoogleBtn
-          text="REGISTRATE CON GOOGLE"
-          onPress={() => navigation.navigate("Register1")}
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>¡Bienvenido a SmartShop!</Text>
+        <Image style={styles.image} source={require("../../assets/logo.png")} />
+        <View style={styles.buttons}>
+          <SessionBtn
+            text="INICIAR SESIÓN"
+            onPress={() => navigation.navigate("Login")}
+          />
+          <NormalBtn
+            text="REGISTRARME"
+            onPress={() => navigation.navigate("Register1")}
+          />
+          <GoogleBtn
+            text="REGISTRATE CON GOOGLE"
+            onPress={() => navigation.navigate("Register1")}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -28,16 +30,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: "5%",
+    height: 800,
   },
   buttons: {
     alignSelf: "center",
     alignItems: "center",
-    marginTop: 150,
+    marginTop: 189,
   },
   title: {
     alignSelf: "center",
     fontSize: 24,
     fontWeight: "500",
-    marginTop: "5%",
+    marginTop: 89,
+    height: 53,
+  },
+  image: {
+    top: 40,
+    alignSelf: "center",
+    height: 179,
+    width: 284,
   },
 });

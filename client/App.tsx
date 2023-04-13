3ceb05d2f3b9aa26c4f4e30529a93b9d9487c40e
tomasAybar <aybar.t4@gmail.com>
2023-04-13 14:42:09 -0300

    feat: creación de "useCounter"
    Se creó el hook useContador para utilizar la funcionalidad del contador en distintas áreas.

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StackRoutes } from "./src/navigation/StackRoutes";
+import { ScanScreen } from "./src/screens/ScanScreen";
 
 export default function App() {
-  return <StackRoutes />;
+  return <ScanScreen />;
 }
--- client/src/components/CounterComponent.tsx
+++ client/src/components/CounterComponent.tsx
@@ -1,37 +1,28 @@
 import { View, TouchableOpacity, Text } from "react-native";
-import { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 
-type Props = {
-  updateCounter: (counter: number) => void;
-};
+interface Props {
+  decrementCounter: () => void;
+  incrementCounter: () => void;
+  counter: number;
+}
 
-const CounterComponent = (props: Props) => {
-  const [counter, setCounter] = useState(1);
-
-  const incrementCounter = () => {
-    setCounter(counter + 1);
-    props.updateCounter(counter + 1);
-  };
-
-  const decrementCounter = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-      props.updateCounter(counter + 1);
-    }
-  };
-
+const CounterComponent = ({
+  counter,
+  decrementCounter,
+  incrementCounter,
+}: Props) => {
   return (
     <View className="flex flex-row w-20 h-8 bg-counter justify-around items-center rounded-xl">
       <View className="w-6 h-6 rounded-full bg-gray-200 items-center justify-center">
-        <TouchableOpacity onPress={incrementCounter}>
-          <Ionicons name="add" size={16} color="black" />
+        <TouchableOpacity onPress={decrementCounter}>
+          <Ionicons name="md-remove" size={18} color="black" />
         </TouchableOpacity>
       </View>
       <Text>{counter}</Text>
       <View className="w-6 h-6 rounded-full bg-gray-200 items-center justify-center">
-        <TouchableOpacity onPress={decrementCounter}>
-          <Ionicons name="md-remove" size={18} color="black" />
+        <TouchableOpacity onPress={incrementCounter}>
+          <Ionicons name="add" size={16} color="black" />
         </TouchableOpacity>
       </View>
     </View>

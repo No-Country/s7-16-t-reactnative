c50f31c5f62cf2e3f48b18869340afbe6615dd1f
Juan Fernandez <jmf798@gmail.com>
2023-04-14 17:34:21 -0300

    feat: business entity

---
+++ server/src/entity/business/services.ts
@@ -0,0 +1,32 @@
+import { Business, BusinessModel } from './model';
+
+export async function createBusiness(business: Business) {
+  return BusinessModel.create(business);
+}
+
+export async function getBusinessList() {
+  return BusinessModel.find({});
+}
+
+export async function getBusiness(id: String) {
+  const responseBusiness = await BusinessModel.findOne({ _id: id });
+  if (!responseBusiness) {
+    throw new Error(`Business ${id} not found`);
+  }
+  return responseBusiness;
+}
+
+export async function updateBusiness(id: string, data: Business) {
+  const responseBusiness = await BusinessModel.findOneAndUpdate({ _id: id }, data, {
+    new: true,
+  });
+  return responseBusiness;
+}
+
+export async function deleteBusiness(id: string) {
+  const responseBusiness = await BusinessModel.deleteOne({ _id: id });
+  if (!responseBusiness.deletedCount) {
+    throw new Error(`Product ${id} not found`);
+  }
+  return responseBusiness;
+}

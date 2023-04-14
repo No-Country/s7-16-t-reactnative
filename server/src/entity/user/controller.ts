7b6336d0f7bc8bb70fb97e5ef5ad64e746b10fd8
Miguel Vera <miguelverap01@gmail.com>
2023-04-14 18:18:19 -0500

    resolve conflicts user update

--- server/src/entity/user/controller.ts
+++ server/src/entity/user/controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
+import { StatusCodes } from 'http-status-codes';
 import { UserModel } from "./model";
-
 
 export const getAllUsers = async (req: Request, res: Response) => {
   const { limit = 5, from = 0 } = req.query;

0f4f3498e6ab7ee4d243f634d12a5493a1921863
Victor Romero Juarez <victortecno98@gmail.com>
2023-04-19 10:10:26 -0600

    feat: provissional process payment mercadopago

--- client/src/hooks/useLogin.tsx
+++ client/src/hooks/useLogin.tsx
@@ -2,6 +2,8 @@ import { useNavigation } from "@react-navigation/native";
 import { Login } from "../utils/api/smartShopDB";
 import { UseUserStore } from "../store/UserStore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useLoader } from "./useLoader";
+import { useState } from "react";
 
 export interface Values {
   email: string;
@@ -12,22 +14,40 @@ export const useLogin = () => {
   const navigation = useNavigation();
 
   const setUser = UseUserStore((state) => state.setUser);
-
+  const { closeLoader, openLoader, isLoading } = useLoader();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
   const handleSubmit = async (values: Values) => {
+    openLoader();
     console.log(values);
 
     const res = await Login(values);
-    console.log(res);
 
     if (res && res.status === 200 && res.data) {
       setUser(res.data.userResponse);
       await AsyncStorage.setItem("token", res.data.token);
-      // console.log(res.data.token);
       navigation.navigate("StackNavigation" as never);
+    } else {
+      setModalVisible(true);
     }
+
+    closeLoader();
+    // console.log(values);
+
+    // const res = await Login(values);
+    // console.log(res);
+
+    // if (res && res.status === 200 && res.data) {
+    //   setUser(res.data.userResponse);
+    //   await AsyncStorage.setItem("token", res.data.token);
+    //   // console.log(res.data.token);
+    //   navigation.navigate("StackNavigation" as never);
+    // }
   };
 
   return {
     handleSubmit,
+    isLoading,
+    modalVisible,
+    setModalVisible,
   };
 };
